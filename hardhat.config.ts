022481c031a9bdec25bbe371615b38a73e2c80b2
Michydev <michyaraque@gmail.com>
2022-08-29 16:38:28 +0200

    refactor: :bug: fixed the bug of not being able to use random private keys

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -11,22 +11,45 @@ import "solidity-coverage";
 import "hardhat-gas-reporter";
 import "hardhat-deploy";
 import "./tasks";
+import { ethers } from "ethers";
 
+/* An array of private keys. */
 const private_keys: string[] = [
-  process.env.PRIVATE_KEY_DEPLOYER as string
+  process.env.PRIVATE_KEY_DEPLOYER as string,
 ];
 
+/* Adding a custom signer to the hardhat environment. */
 extendEnvironment((hre: any) => {
-  const formatedCustomKeys = private_keys.map((private_key: string) => {
-    return new hre.ethers.Wallet(private_key, hre.ethers.provider)
-  })
-  hre.customSigners = formatedCustomKeys;
+  if(private_keys[0]) {
+    const formatedCustomKey = hre.ethers.Wallet(private_keys[0], hre.ethers.provider)
+    hre.customSigners = formatedCustomKey;
+  }
 });
 
+/* Extending the HardhatUserConfig interface. */
 interface FullHardhatUserConfig extends HardhatUserConfig {
   etherscan: {
-    apiKey: any;
+    apiKey: Record<string, any>;
   }
+}
+
+/**
+ * It creates a random wallet.
+ * @returns A random wallet address.
+ */
+const createRandomWalletOnInit = () => {
+  let random_address: ethers.Wallet = ethers.Wallet.createRandom();
+  return random_address;
+}
+
+/**
+ * If there is a private key in the private_keys array, return it, otherwise return the private key of
+ * the wallet created by the createRandomWalletOnInit() function.
+ * @returns The private key of the first wallet in the array of wallets.
+ */
+const usePrivateKeyOrMockKey = () => {
+  let private_key = private_keys[0];
+  return private_key ? private_key : createRandomWalletOnInit().privateKey;
 }
 
 const config: FullHardhatUserConfig = {
@@ -48,16 +71,16 @@ const config: FullHardhatUserConfig = {
       } */
     },
     bsc: {
-      accounts: process.env.BSC_DEPLOYER_KEY !== undefined ? [process.env.BSC_DEPLOYER_KEY] : [],
+      accounts: [process.env.BSC_DEPLOYER_KEY ? process.env.BSC_DEPLOYER_KEY : createRandomWalletOnInit().privateKey],
       url: `https://bsc-dataseed1.binance.org`,
     },
     bscTestnet: {
       url: `https://data-seed-prebsc-1-s1.binance.org:8545`,
-      accounts: [private_keys[0]]
+      accounts: [usePrivateKeyOrMockKey()]
     },
     rinkeby: {
       url: `https://rinkeby.infura.io/v3/${process.env.INFURA_API_KEY}`,
-      accounts: [private_keys[0]],
+      accounts: [usePrivateKeyOrMockKey()],
       chainId: 4,
       gasPrice: 5000000000,
       gasMultiplier: 2
@@ -70,7 +93,10 @@ const config: FullHardhatUserConfig = {
     currency: "USD",
   },
   etherscan: {
-    apiKey: process.env.BSCSCAN_API_KEY
+    apiKey: {
+      bscscan: process.env.BSCSCAN_API_KEY,
+      ethereum: process.env.ETHERSCAN_API_KEY,
+    }
   },
 };
 
